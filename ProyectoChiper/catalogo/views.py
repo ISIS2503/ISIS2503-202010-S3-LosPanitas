b1f221fca16cc49c7689b47db6eff4eeb23a8bde
Ubuntu <ubuntu@ip-172-31-89-137.ec2.internal>
2020-05-08 01:49:43 +0000

    estamos muy cracks no te danes porfa

--- ProyectoChiper/catalogo/views.py
+++ ProyectoChiper/catalogo/views.py
@@ -8,39 +8,49 @@ from .catalogo_logic.catalogo_logic import get_products, create_producto
 from django.contrib.auth.decorators import login_required
 from ProyectoChiper.auth0backend import getRole
 
-#role = getRole(request)
-#    if role == "Supervisor":
 
-
-#@login_required
+@login_required
 def catalogo_list(request):
     catalogo = get_products()
+    gerente= False
+    role= getRole(request)
+    if role=="Gerencia Chiper":
+        gerente= True
+
     context = {
-        'catalogo_list': catalogo
+    'catalogo_list': catalogo,
+    'gerente': gerente,
     }
     return render(request, 'Catalogo/catalogo.html', context)
 
+
+@login_required
 def producto_create(request):
-    if request.method == 'POST':
-        form = ProductoForm(request.POST)
-        if form.is_valid():
-            rta=create_producto(form)
-            if rta=='Producto creado':
-                messages.add_message(request, messages.SUCCESS, rta)
-                print(messages)
-                return HttpResponseRedirect(reverse('productoCreate'))
+    role=getRole(request)
+    if role=="Gerencia Chiper":
+        if request.method == 'POST':
+            form = ProductoForm(request.POST)
+            if form.is_valid():
+                rta=create_producto(form)
+                if rta=='Producto creado':
+                    messages.add_message(request, messages.SUCCESS, rta)
+                    print(messages)
+                    return HttpResponseRedirect(reverse('productoCreate'))
+                else:
+                    messages.add_message(request, messages.ERROR, rta)
+                    print(messages)
+                    messages.error(request, "Error")
+                    return HttpResponseRedirect(reverse('productoCreate'))
             else:
-                messages.add_message(request, messages.ERROR, rta)
-                print(messages)
-                messages.error(request, "Error")
-                return HttpResponseRedirect(reverse('productoCreate'))
+                print(form.errors)
         else:
-            print(form.errors)
+            form = ProductoForm()
+
+        context = {
+            'form': form,
+            'gerente': True,
+        }
+
+        return render(request, 'Catalogo/productoCreate.html', context)
     else:
-        form = ProductoForm()
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'Catalogo/productoCreate.html', context)
+        return HttpResponse("Unauthorized User")
