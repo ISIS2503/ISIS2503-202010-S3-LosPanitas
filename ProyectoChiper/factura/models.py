a905e1e207029c5ac54413a46b7c5da549a4db27
Juan Lucas Ibarra Sierra <jl.ibarra@uniandes.edu.co>
2020-05-29 12:04:23 -0500

    Actualizado Factura

--- ProyectoChiper/factura/models.py
+++ ProyectoChiper/factura/models.py
@@ -6,5 +6,11 @@ class Factura(models.Model):
     fecha = models.DateTimeField(auto_now_add=False)
     monto = models.FloatField()
 
+class ProductoFactura(models.Model):
+    nombre = models.CharField(max_length=30)
+    precio = models.FloatField(null=True, blank=True, default=None)
+    tipo = models.CharField(max_length=20)
+    catalogo = models.ForeignKey(Factura, on_delete=models.CASCADE)
+
     def __str__(self):
         return '%s %s'.format(self.fecha, self.monto)
