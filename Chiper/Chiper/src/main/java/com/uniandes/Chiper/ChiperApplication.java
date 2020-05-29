a2f79df5541c1719c6646f20addcc6d18d0469c5
SantiagoE <s.estupinan@uniandes.edu.co>
2020-05-29 13:59:34 -0500

    microservicio de java con list y create

---
+++ Chiper/Chiper/src/main/java/com/uniandes/Chiper/ChiperApplication.java
@@ -0,0 +1,13 @@
+package com.uniandes.Chiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiperApplication.class, args);
+	}
+
+}
---
+++ Chiper/Chiper/src/test/java/com/uniandes/Chiper/ChiperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniandes.Chiper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
